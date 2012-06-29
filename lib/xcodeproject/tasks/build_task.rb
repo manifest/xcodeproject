317eabb5c378159a1122d08b1f5937fcafc88afd
Andrey Nesterov <ae.nesterov@gmail.com>
2012-06-29 17:02:32 +0400

    added ability to change build directory

--- lib/xcodeproject/tasks/build_task.rb
+++ lib/xcodeproject/tasks/build_task.rb
@@ -4,16 +4,25 @@ module XcodeProject
 	module Tasks
 		class BuildTask < XcodeBuild::Tasks::BuildTask
 			attr_accessor :with_build_opts
+			attr_accessor :build_to
 
 			def initialize (project, namespace = nil, &block)
 				namespace ||= project.name
 				super(namespace, &block)
 
-				@with_build_opts  ||= []
-				@formatter        ||= XcodeBuild::Formatters::ProgressFormatter.new
-
 				@project_name       = project.bundle_path.basename.to_s
 				@invoke_from_within = project.bundle_path.dirname
+
+				@formatter        ||= XcodeBuild::Formatters::ProgressFormatter.new
+				@with_build_opts  ||= []
+				
+				unless @build_to.nil?
+					build_tmp_to = Pathname.new(@build_to).join('.tmp')
+
+					@with_build_opts << %{ CONFIGURATION_BUILD_DIR="#{@build_to}" }
+					@with_build_opts << %{ CONFIGURATION_TEMP_DIR="#{build_tmp_to}" }
+					@with_build_opts << %{ SYMROOT="#{build_tmp_to}" }
+				end
 			end
 
 		private
